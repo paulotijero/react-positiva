e8ae9166d8225ef6066d20402cb0cdc473d0f5ef
paulotijero <paulotijero@gmail.com>
2019-08-01 22:22:04 -0500

    Add global styles and font

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+/** @jsx jsx */
+import React from "react";
+import { jsx } from "@emotion/core";
+
+import Home from "../views/home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
