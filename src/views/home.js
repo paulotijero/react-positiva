e1687772c5fe7ec556fb296a563bf18257253efd
paulotijero <paulotijero@gmail.com>
2019-08-01 23:39:39 -0500

    Add footer

---
+++ src/views/home.js
@@ -0,0 +1,11 @@
+/** @jsx jsx */
+import React from "react";
+import { jsx } from "@emotion/core";
+
+import Footer from "../components/footer";
+
+function Home() {
+  return <Footer />;
+}
+
+export default Home;
